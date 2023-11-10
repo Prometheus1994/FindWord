ed42bdce6fa139237faa7a872898009ab1d3156f
Prometheus1994 <48414632+Prometheus1994@users.noreply.github.com>
2023-11-10 04:07:29 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+import nltk
+from nltk.corpus import words as nltk_words
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/find_words', methods=['POST'])
+def find_words():
+    word_list = nltk_words.words()
+    input_word = request.form['word']
+    solutions = []
+    for word in word_list:
+        counter = 0
+        if len(word) != len(input_word):
+            continue
+        else:
+            for i in range(len(word)):
+                if word[i] == input_word[i] or input_word[i] == '_':
+                    counter += 1
+                else:
+                    break
+        if counter == len(input_word):
+            solutions.append(word)
+    return render_template('index.html', solutions=solutions)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
