2130ede0dfa7cb72099d1dc1424135ba772c3df3
Prometheus1994 <48414632+Prometheus1994@users.noreply.github.com>
2023-11-16 17:20:30 +0100

    Add files via upload

--- CommentedCode.py
+++ CommentedCode.py
@@ -1,40 +1,54 @@
-# Import the nltk library
+# Import necessary libraries
 import nltk
 
-# Download the required data (uncomment the line below and run it in your local environment if needed)
-# nltk.download()
+# Uncomment the line below to download the 'words' resource
+# nltk.download('words')
 
-# Import the word list from the nltk corpus
+from flask import Flask, render_template, request
 from nltk.corpus import words as nltk_words
 
-# Get the list of words from nltk
-word_list = nltk_words.words()
+# Create a Flask application with a static URL path
+app = Flask(__name__, static_url_path='/static')
 
-# Define the input word with missing letters represented by underscores
-input_word = "ba_a_a"
+# Define the home route
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-# Initialize an empty list to store solutions
-solutions = []
-
-# Iterate through each word in the word list
-for word in word_list:
-    # Initialize a counter to keep track of matching characters
-    counter = 0
+# Define the find_words route for handling word search
+@app.route('/find_words', methods=['POST'])
+def find_words():
+    # Load the words from NLTK
+    word_list = nltk_words.words()
     
-    # Check if the word's length matches the input word's length
-    if len(word) != len(input_word):
-        continue  # Skip to the next word if the lengths do not match
-    else:
-        # Iterate through each character in the word and input word
-        for i in range(len(word)):
-            if word[i] == input_word[i] or input_word[i] == '_':
-                counter += 1
-            else:
-                break  # Break the loop if a character doesn't match or is not a placeholder
+    # Get the input word from the form
+    input_word = request.form['word']
     
-    # If the counter equals the length of the input word, all characters match
-    if counter == len(input_word):
-        solutions.append(word)  # Add the word to the list of solutions
+    # Initialize an empty list to store solutions
+    solutions = []
+    
+    # Iterate over each word in the word list
+    for word in word_list:
+        counter = 0
+        
+        # Check if the length of the word matches the input word
+        if len(word) != len(input_word):
+            continue
+        else:
+            # Compare each character in the word and input_word
+            for i in range(len(word)):
+                if word[i] == input_word[i] or input_word[i] == '_':
+                    counter += 1
+                else:
+                    break
+        
+        # If the counter matches the length of the input word, add it to solutions
+        if counter == len(input_word):
+            solutions.append(word)
+    
+    # Render the template with the solutions
+    return render_template('index.html', solutions=solutions)
 
-# Print the list of solutions
-print(solutions)
+# Run the Flask application in debug mode
+if __name__ == '__main__':
+    app.run(debug=True)
